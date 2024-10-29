91ef99a5bc8c683ab9b09e14e5273c752b11439e
Jess Nguyen <nguyjess@gmail.com>
2024-10-29 11:27:46 -0400

    added the alternative way of looking at json

--- examples/convert_rust_json_to_json/src/main.rs
+++ examples/convert_rust_json_to_json/src/main.rs
@@ -1,10 +1,19 @@
 use colored::Colorize;
 use regex::Regex;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
 
+#[derive(Debug, Serialize, Deserialize)]
+struct DatabaseItem {
+    name: String,
+    age: i32,
+    city: String,
+}
 fn main() {
+    // serde_to_string();
     let contents: String = read_file();
     let basic_json = convert_rust_serde_to_general_json(&contents);
     write_file(basic_json);
@@ -78,4 +87,20 @@ fn write_file(basic_json: String) {
         fs::read_to_string(file_path).expect("Should have been able to READ the NEW file");
 
     println!("\nNew file has text:\n{}\n", contents.yellow());
+}
+
+fn serde_to_string() {
+    let item = DatabaseItem {
+        name: "Alice".to_string(),
+        age: 30,
+        city: "New York".to_string(),
+    };
+    println!("Item: {}", format!("{:?}", item).blue());
+
+    let slice_string_in_json_format = serde_json::to_string(&item);
+
+    println!(
+        "Slice String in JSON format: {}",
+        format!("{:?}", slice_string_in_json_format).red()
+    );
 }
